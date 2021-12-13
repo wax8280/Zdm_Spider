40b04b3c33b27b1675e93c3fd3e26e2a2400f246
wax8280 <wax8280@163.com>
2021-12-13 12:16:53 +0800

    update

--- flask_blueprint/yuehou.py
+++ flask_blueprint/yuehou.py
@@ -53,6 +53,67 @@ def update_now():
     return '正在更新，请稍后...'
 
 
+def get_hot(hour):
+    article_query = Article.select().where(Article.timesort > int(time.time()) - 60 * 60 * hour).order_by(
+        Article.article_score.desc()).limit(300)
+    update_time = timestamp_to_str(Article.select(fn.MAX(Article.timesort)).scalar())
+    result = []
+
+    for item in article_query:
+        a = {
+            'article_content': item.article_content,
+            'article_id': item.article_id,
+            'article_link': item.article_link,
+            'article_url': item.article_url,
+            'article_mall': item.article_mall,
+            'article_pic_url': item.article_pic_url,
+            'article_price': item.article_price,
+            'article_rating': item.article_rating,
+            'article_title': item.article_title,
+            'article_top_category': item.article_top_category,
+            'price_level': item.price_level,
+            'timesort': item.timesort,
+            'zhifa_tag': item.zhifa_tag,
+            'article_collection': item.article_collection,
+            'article_comment': item.article_comment,
+            'article_score': int(item.article_score),
+            'local_article_pic_url': item.local_article_pic_url,
+            'stock_status_note': item.stock_status_note,
+            'time_str': timestamp_to_str(item.timesort, format_str="%m-%d %H:%M")
+        }
+        result.append(a)
+    return result, update_time
+
+
+@yuehou.route('/hot3', methods=['GET'])
+def hot3():
+    start_t = time.time()
+    result, update_time = get_hot(3)
+    return render_template('hot.html', articles=result, update_time=update_time[5:],
+                           handle_time=str(float(str(time.time() - start_t)[:5]) * 1000) + 'ms')
+
+
+@yuehou.route('/hot6', methods=['GET'])
+def hot6():
+    start_t = time.time()
+    result, update_time = get_hot(6)
+    return render_template('hot.html', articles=result, update_time=update_time[5:],
+                           handle_time=str(float(str(time.time() - start_t)[:5]) * 1000) + 'ms')
+
+@yuehou.route('/hot12', methods=['GET'])
+def hot12():
+    start_t = time.time()
+    result, update_time = get_hot(12)
+    return render_template('hot.html', articles=result, update_time=update_time[5:],
+                           handle_time=str(float(str(time.time() - start_t)[:5]) * 1000) + 'ms')
+
+@yuehou.route('/hot24', methods=['GET'])
+def hot24():
+    start_t = time.time()
+    result, update_time = get_hot(24)
+    return render_template('hot.html', articles=result, update_time=update_time[5:],
+                           handle_time=str(float(str(time.time() - start_t)[:5]) * 1000) + 'ms')
+
 @yuehou.route('/get_article', methods=['GET'])
 @auth.login_required
 def get_article():
@@ -111,5 +172,5 @@ def get_article():
 
         article_ids_str = '^'.join(article_ids)
 
-    return render_template('base.html', articles=result, article_ids_str=article_ids_str, update_time=update_time[5:],
-                           handle_time=str(time.time() - start_t)[:5])
+    return render_template('yuehou.html', articles=result, article_ids_str=article_ids_str, update_time=update_time[5:],
+                           handle_time=str(float(str(time.time() - start_t)[:5]) * 1000) + 'ms')
